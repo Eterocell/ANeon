5e5b61fc678fd5daff47687301d0584147918a62
Eterocell <eterocell@outlook.com>
2024-07-24 09:53:07 +0800

    fix(native): Support 16KB page size devices on Android 15+
    Signed-off-by: Eterocell <eterocell@outlook.com>

--- build-logic/convention/src/main/kotlin/build-logic.android.native.gradle.kts
+++ build-logic/convention/src/main/kotlin/build-logic.android.native.gradle.kts
@@ -5,7 +5,10 @@ configureAndroidCommon {
 
     defaultConfig.externalNativeBuild {
         cmake {
-            arguments += "-DANDROID_STL=c++_shared"
+            arguments += listOf(
+                "-DANDROID_STL=c++_shared",
+                "-DANDROID_SUPPORT_FLEXIBLE_PAGE_SIZES=ON" // Support 16KB memory page size devices
+            )
             cFlags += "-std=c18"
             cppFlags += "-std=c++20"
             abiFilters("arm64-v8a", "armeabi-v7a", "x86", "x86_64")
