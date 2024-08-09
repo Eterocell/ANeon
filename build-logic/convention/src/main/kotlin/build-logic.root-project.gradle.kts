6b6f322d9b6145f2d8d1a55c26f08add8418c52b
Eterocell <eterocell@outlook.com>
2024-08-09 16:13:23 +0800

    chore(build): Make clean task also remove cmake generated files
    Signed-off-by: Eterocell <eterocell@outlook.com>

--- build-logic/convention/src/main/kotlin/build-logic.root-project.gradle.kts
+++ build-logic/convention/src/main/kotlin/build-logic.root-project.gradle.kts
@@ -7,3 +7,10 @@ val wrapper: Wrapper by tasks.named<Wrapper>("wrapper") {
     gradleVersion = "8.9"
     distributionType = Wrapper.DistributionType.ALL
 }
+
+tasks.clean {
+    allprojects.forEach {
+        delete(it.layout.buildDirectory)
+        delete(files("${it.layout.projectDirectory.asFile.absolutePath}/.cxx"))
+    }
+}
