da0334f856812d9302487412a3ce1225beb9aeda
Eterocell <eterocell@outlook.com>
2024-09-10 09:28:07 +0800

    chore(build): Gradle 8.10.1 in wrapper task, NDK r27b
    Signed-off-by: Eterocell <eterocell@outlook.com>

--- build-logic/convention/src/main/kotlin/build-logic.android.native.gradle.kts
+++ build-logic/convention/src/main/kotlin/build-logic.android.native.gradle.kts
@@ -1,7 +1,7 @@
 import com.eterocell.gradle.dsl.configureAndroidCommon
 
 configureAndroidCommon {
-    ndkVersion = "27.0.12077973"
+    ndkVersion = "27.1.12297006"
 
     defaultConfig.externalNativeBuild {
         cmake {
