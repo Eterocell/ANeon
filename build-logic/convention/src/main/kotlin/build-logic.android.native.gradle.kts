8939adc2d76212931ad6443fd90bd866605821bd
Eterocell <eterocell@outlook.com>
2025-01-06 10:45:23 +0800

    fix(deps): ndk r28 rc2, cmake 3.31.1, c23, c++23, ktlint 1.5.0
    Signed-off-by: Eterocell <eterocell@outlook.com>

--- build-logic/convention/src/main/kotlin/build-logic.android.native.gradle.kts
+++ build-logic/convention/src/main/kotlin/build-logic.android.native.gradle.kts
@@ -1,7 +1,7 @@
 import com.eterocell.gradle.dsl.configureAndroidCommon
 
 configureAndroidCommon {
-    ndkVersion = "27.1.12297006"
+    ndkVersion = "28.0.12674087"
 
     defaultConfig.externalNativeBuild {
         cmake {
@@ -9,14 +9,14 @@ configureAndroidCommon {
                 "-DANDROID_STL=c++_shared",
                 "-DANDROID_SUPPORT_FLEXIBLE_PAGE_SIZES=ON", // Support 16KB memory page size devices
             )
-            cFlags += "-std=c18"
-            cppFlags += "-std=c++20"
+            cFlags += "-std=c23"
+            cppFlags += "-std=c++23"
             abiFilters("arm64-v8a", "armeabi-v7a", "x86", "x86_64")
         }
     }
     externalNativeBuild {
         cmake {
-            version = "3.30.3"
+            version = "3.31.1"
         }
     }
 }
