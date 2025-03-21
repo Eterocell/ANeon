6ff7f3594f6c002b721b186fc63ebf762afae503
Eterocell <eterocell@outlook.com>
2025-03-21 11:02:20 +0800

    chore(deps): import version catalog update plugin, update dependencies
    Signed-off-by: Eterocell <eterocell@outlook.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,15 +1,12 @@
 pluginManagement {
     includeBuild("build-logic")
     repositories {
-        gradlePluginPortal {
-            content {
-                includeGroupByRegex("com.gradle.*")
-            }
-        }
+        gradlePluginPortal()
         google()
         mavenCentral()
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
