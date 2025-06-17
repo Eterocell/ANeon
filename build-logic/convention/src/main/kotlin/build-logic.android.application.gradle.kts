3524e910882310528dafd5556314a3a3e867513f
Eterocell <eterocell@outlook.com>
2025-06-17 12:27:44 +0800

    fix: refresh project configs
    Signed-off-by: Eterocell <eterocell@outlook.com>

--- build-logic/convention/src/main/kotlin/build-logic.android.application.gradle.kts
+++ build-logic/convention/src/main/kotlin/build-logic.android.application.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 configureAndroidApplication {
     defaultConfig {
         applicationId = extra["aneon.project.group"].toString()
-        targetSdk = 35
+        targetSdk = 36
 
         versionCode = extra["aneon.project.version.code"].toString().toInt()
         versionName = extra["aneon.project.version.name"].toString()
--- build-logic/convention/src/main/kotlin/build-logic.android.base.gradle.kts
+++ build-logic/convention/src/main/kotlin/build-logic.android.base.gradle.kts
@@ -7,8 +7,8 @@ plugins {
 
 configureAndroidCommon {
     namespace = androidNamespace
-    compileSdk = 35
-    buildToolsVersion = "35.0.0"
+    compileSdk = 36
+    buildToolsVersion = "36.0.0"
     defaultConfig {
         minSdk = 24
     }
